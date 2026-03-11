b1c7a95149de498c00b4a1c321d0977b2ad15351
truongduybenlop10a12 <truongduyben123.az@gmail.com>
2026-03-11 09:28:10 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+<meta charset="UTF-8">
+<title>Đăng nhập</title>
+
+<link rel="stylesheet" href="reset.css">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="tabs">
+        <span class="active">Đăng nhập</span>
+        <span>Đăng ký</span>
+    </div>
+
+    <div class="avatar">
+        <img src="avatar.png" alt="avatar">
+    </div>
+
+    <form>
+
+        <input type="email" placeholder="Email" required>
+
+        <input type="password" placeholder="Mật khẩu" required>
+
+        <button type="submit">Đăng nhập</button>
+
+    </form>
+
+    <div class="forgot">
+            <a href="a">Quên mật khẩu?</a>
+        </div>
+
+        </div>
+
+        </body>
+</html>
---
+++ register.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+<meta charset="UTF-8">
+<title>Đăng ký</title>
+
+<link rel="stylesheet" href="reset.css">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="tabs">
+        <span class="login">ĐĂNG NHẬP</span>
+        <span class="active">ĐĂNG KÝ</span>
+    </div>
+
+    <div class="avatar">
+        <img src="avatar.png" alt="avatar">
+    </div>
+
+    <form>
+
+        <input type="text" placeholder="Họ tên" required>
+
+        <input type="email" placeholder="Email" required>
+
+        <input type="password" placeholder="Mật khẩu" required>
+
+        <input type="password" placeholder="Xác nhận mật khẩu" required>
+
+        <button type="submit">Đăng ký</button>
+
+        <div class="forgot">
+            <a href="a">Quên mật khẩu?</a>
+        </div>
+
+    </form>
+
+</div>
+
+</body>
+</html>
